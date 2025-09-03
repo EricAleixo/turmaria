55d6bfbe7aec545cc85d577cb2d90c35f31b053f
devLuanMartins <luanmm.14.27@gmail.com>
2025-09-03 09:06:51 -0300

    erro proximo resolvido

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_02_120538) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_03_115335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -84,6 +84,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_02_120538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["nome"], name: "index_escolas_on_nome", unique: true
+  end
+
+  create_table "nota", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "professors", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
