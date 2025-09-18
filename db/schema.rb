b1ca729a97a1212d8167351e7279a615eddbeb71
devLuanMartins <luanmm.14.27@gmail.com>
2025-09-18 11:30:49 -0300

    login melhorado | show de alunos

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
+ 
 ActiveRecord::Schema[7.1].define(version: 2025_09_15_112203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
