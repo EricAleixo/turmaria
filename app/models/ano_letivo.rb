ca792bf17a0630ea31a4f9676888fab1e9273f0e
devLuanMartins <luanmm.14.27@gmail.com>
2025-09-26 09:34:01 -0300

    anoletivo alterado

--- app/models/ano_letivo.rb
+++ app/models/ano_letivo.rb
@@ -3,7 +3,12 @@ class AnoLetivo < ApplicationRecord
   accepts_nested_attributes_for :turmas
   belongs_to :escola
 
-  validates :ano, presence: true, uniqueness: true, length: {maximum: 4}
+  validates :ano,
+    presence: true,
+    uniqueness: { scope: :escola_id, message: "Já existe um ano letivo com esse número para esta escola." },
+    length: {maximum: 4}
+
   validates :data_inicio, presence: true, length: {maximum: 11}
   validates :data_fim, presence: true, length: {maximum: 11}
 end
+ 
