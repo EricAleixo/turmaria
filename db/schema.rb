fbb5c437c78b95f8ad829025986281dd76c343a5
ericAleixo <ericaleixo66@gmail.com>
2025-10-02 08:07:36 -0300

    MERGE: developer updates

--- db/schema.rb
+++ db/schema.rb
@@ -167,13 +167,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_01_122243) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "materia", force: :cascade do |t|
-    t.string "nome"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["nome"], name: "index_materia_on_nome", unique: true
-  end
-
   create_table "nota", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,57 @@
-puts "Criando estado e cidade..."
-estado1 = Estado.create!(nome: "São Paulo 2")
-cidade1 = Cidade.create!(nome: "Campinas 2", estado: estado1)
+puts "Limpando registros antigos..."
+SuperAdmin.delete_all
+Admin.delete_all
+Professor.delete_all
+Escola.delete_all
+Cidade.delete_all
+Estado.delete_all
 
-puts "Criando escola..."
-escola1 = Escola.create!(
-  nome: "Escola Municipal Exemplo 1",
+puts "Criando Estado e Cidade..."
+estado = Estado.create!(nome: "São Paulo")
+cidade = Cidade.create!(nome: "Campinas", estado: estado)
+
+puts "Criando Escola..."
+escola = Escola.create!(
+  nome: "Escola Municipal Exemplo",
   cnpj: "12.345.678/0001-98",
   telefone: "1234-5677",
-  email: "contato@escolaexemplo1.com",
-  site: "www.escolaexemplo1.com",
+  email: "contato@escolaexemplo.com",
+  site: "www.escolaexemplo.com",
   tipo: "privada"
 )
 
-puts "Cidade e escola criadas com sucesso!"
+puts "Criando SuperAdmin..."
+super_admin = SuperAdmin.create(
+  email: "superadmin@teste.com",
+  password: "123456",
+  confirmed_at: Time.current
+)
+puts super_admin.persisted? ? "SuperAdmin criado!" : "Erro: #{super_admin.errors.full_messages.join(", ")}"
+
+puts "Criando Admin..."
+admin = Admin.create(
+  email: "admin@teste.com",
+  password: "123456",
+  confirmed_at: Time.current
+)
+puts admin.persisted? ? "Admin criado!" : "Erro: #{admin.errors.full_messages.join(", ")}"
+
+puts "Criando Professores..."
+10.times do |i|
+  prof = Professor.create(
+    email: "professor#{i+1}@teste.com",
+    nome: "professor#{i+1}",
+    cpf: rand(100_000_000..999_999_999).to_s, # gera 9 dígitos aleatórios
+    password: "123456",
+    confirmed_at: Time.current,
+    escola: escola
+  )
+
+  if prof.persisted?
+    puts "Professor #{i+1} criado!"
+  else
+    puts "Erro Professor #{i+1}: #{prof.errors.full_messages.join(", ")}"
+  end
+end
+
+puts "🌱 Seeds finalizados com sucesso!"
